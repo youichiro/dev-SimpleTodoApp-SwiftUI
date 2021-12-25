71000fa03ec8a7da6f8a6194d88a353957a18562
youichiro <cinnamon416@gmail.com>
2021-12-25 22:23:22 +0900

    Initial Commit

---
+++ dev-SimpleTodoApp/dev_SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_SimpleTodoAppApp.swift
+//  dev-SimpleTodoApp
+//
+//  Created by 小川耀一朗 on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct dev_SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
